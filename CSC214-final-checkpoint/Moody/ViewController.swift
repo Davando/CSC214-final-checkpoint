19d741a914171271367c2a3ff92498467bd30e4c
yanyujianshen <ljh274163788@gmail.com>
2020-10-31 21:20:04 -0700

    finished the view controller, we don't really need any model

--- CSC214-final-checkpoint/Moody/ViewController.swift
+++ CSC214-final-checkpoint/Moody/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var titleOfApp: UILabel!
+    @IBOutlet weak var imageOfMood: UIImageView!
+    @IBOutlet weak var prompt: UILabel!
+    @IBOutlet weak var happyBtn: UIButton!
+    @IBOutlet weak var sadButton: UIButton!
+    @IBOutlet weak var angryButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapHappy(_ sender: Any) {
+        imageOfMood.image = UIImage(named: "happy")
+    }
+    
+    @IBAction func tapSad(_ sender: Any) {
+        imageOfMood.image = UIImage(named: "sad")
+    }
+    
+    @IBAction func tapAngry(_ sender: Any) {
+        imageOfMood.image = UIImage(named: "angry")
+    }
+    
+    
+    
 }
 
