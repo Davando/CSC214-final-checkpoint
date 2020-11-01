ce7bc69c0823f313cba4f2a772bb30363ff43d6c
Sarah Zaman <55359781+SarahZam@users.noreply.github.com>
2020-10-31 23:25:26 -0400

    Add files via upload

---
+++ CSC214-final-checkpoint/Moody/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moody
+//
+//  Created by David Wang on 2020-10-31.
+//  Copyright © 2020 GetSwifty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CSC214-final-checkpoint/Moody/gameModel.swift
@@ -0,0 +1,14 @@
+//
+//  gameModel.swift
+//  Moody
+//
+//  Created by David Wang on 2020-10-31.
+//  Copyright © 2020 GetSwifty. All rights reserved.
+//
+
+import Foundation
+
+//use this class if you need to create any additional game logic
+class gameModel {
+    
+}
